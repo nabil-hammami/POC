3512706718125629cf246f55c8e4902609511964
Nabil <nabilh17@pm.me>
2019-05-14 17:51:05 +0200

    webdriver configured

--- features/step_definitions/UI_menu_test.js
+++ features/step_definitions/UI_menu_test.js
@@ -1,20 +1,27 @@
 const assert = require('assert').strict;
 const { Given, When, Then } = require('cucumber');
-const webdriver = require('selenium-webdriver');
+const { Builder } = require('selenium-webdriver');
+
+async function navigate() {
+    // Webdriver declaration
+    let driver = await new Builder().forBrowser('firefox').build();
+    driver.get('https://nabil-hammami.github.io/POC/');
+    await driver.findElement(By.name('menuHome')).click();
+    //await driver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN);
+    await driver && driver.quit();
+}
+
 
 Given('I visit my website', function () {
-    // Write code here that turns the phrase above into concrete actions
-    return 'pending';
-  });
+    return ;
+});
 
 
 When('I click on HOME button', function () {
-    // Write code here that turns the phrase above into concrete actions
-    return 'pending';
+    navigate();
 });
 
 Then('the HOME page must be displayed', function () {
     // Write code here that turns the phrase above into concrete actions
     return 'pending';
 });
-
