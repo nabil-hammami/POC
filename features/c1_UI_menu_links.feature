01e115298a7632c70f5f53105a6133ffc40fac62
Nabil <nabilh17@pm.me>
2019-05-14 16:55:12 +0200

    before UI test

--- features/c1_UI_menu_links.feature
+++ features/c1_UI_menu_links.feature
@@ -2,6 +2,6 @@ Feature: Menu links
 
     All menu links must open the right page
     Scenario: When I click on HOME menu button, the Home page must be displayed
-        Given I visit https://nabil-hammami.github.io/POC/
+        Given I visit my website
         When I click on HOME button
-        Then the home page must be displayed
+        Then the HOME page must be displayed
---
+++ features/step_definitions/UI_menu_test.js
@@ -0,0 +1,20 @@
+const assert = require('assert').strict;
+const { Given, When, Then } = require('cucumber');
+const webdriver = require('selenium-webdriver');
+
+Given('I visit my website', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
+
+
+When('I click on HOME button', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+
+Then('the HOME page must be displayed', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+
